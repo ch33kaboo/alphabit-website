8bce30e7896480b6c4020539b223590bc506a92b
ch33kaboo <mc.ouzane@esi-sba.dz>
2022-10-19 00:22:13 +0100

    started working on navbar component

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,21 @@
 <script>
+    //importing Modules
     // @ts-nocheck
-    import { Router, Route, Link } from "svelte-navigator";
+    import { Router, Route, Link } from 'svelte-navigator';
+
+    //importing components
+    import Navbar from './lib/Navbar.svelte';
 </script>
 
 <Router>
-    <main class="min-h-screen flex flex-col items-center justify-between">
-        <!-- nav-bar / header component (jcp kisammoh) goes here -->
-        <header>here the header</header>
-    
+    <main class="flex min-h-screen flex-col items-center justify-between">
+        <Navbar />
+
         <main>
             <!-- here goes all the routes & their components inside -->
             here the routes (main content)
         </main>
-    
+
         <footer>
             <!-- here goes the footer -->
             here the footer
