395f854cfa63756b2c1359351f039d2a25f0c054
ch33kaboo <mc.ouzane@esi-sba.dz>
2022-10-18 21:36:58 +0100

    added component with a cool effect on alphabit logo

---
+++ src/lib/AnimatedLogo.svelte
@@ -0,0 +1,53 @@
+<script>
+	import { onMount } from 'svelte';
+
+	let canvas;
+
+	onMount(() => {
+		const ctx = canvas.getContext('2d');
+		let frame = requestAnimationFrame(loop);
+
+		function loop(t) {
+			frame = requestAnimationFrame(loop);
+
+			const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+			for (let p = 0; p < imageData.data.length; p += 4) {
+				const i = p / 4;
+				const x = i % canvas.width;
+				const y = i / canvas.width >>> 0;
+
+				const r = 64 + (128 * x / canvas.width) + (64 * Math.sin(t / 1000));
+				const g = 64 + (128 * y / canvas.height) + (64 * Math.cos(t / 1000));
+				const b = 128;
+
+				imageData.data[p + 0] = r;
+				imageData.data[p + 1] = g;
+				imageData.data[p + 2] = b;
+				imageData.data[p + 3] = 255;
+			}
+
+			ctx.putImageData(imageData, 0, 0);
+		}
+
+		return () => {
+			cancelAnimationFrame(frame);
+		};
+	});
+</script>
+
+<div class="overflow-visible scale-75 md:scale-100">
+	<canvas
+		bind:this={canvas}
+		width={330}
+		height={430}
+	></canvas>
+</div>
+
+<style>
+	canvas {
+		background-color: #666;
+		-webkit-mask: url(../assets/alphabit_logo.png) no-repeat;
+		mask: url(../assets/alphabit_logo.png) no-repeat;
+	}
+</style>
