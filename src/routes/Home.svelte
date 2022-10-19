9f0c9b2603f1b95777ea746f5e90cf4d7fd6caf1
ch33kaboo <mc.ouzane@esi-sba.dz>
2022-10-19 19:10:03 +0100

    minor changes

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,1 +1,7 @@
-this is home route
+<script>
+    import AnimatedLogo from '../lib/AnimatedLogo.svelte';
+</script>
+
+<main class="flex w-full items-center justify-around">
+    <AnimatedLogo />
+</main>
