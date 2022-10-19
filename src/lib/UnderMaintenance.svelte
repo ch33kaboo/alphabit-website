08ef343f7078d79ccb20203afdf8fbf0572a977b
ch33kaboo <mc.ouzane@esi-sba.dz>
2022-10-19 19:06:42 +0100

    added a component for underMaintenance/underConstruction

---
+++ src/lib/UnderMaintenance.svelte
@@ -0,0 +1,12 @@
+<div class="relative">
+    <div
+        class="absolute inset-0 rounded-full bg-slate-600 opacity-50 blur-md"
+    />
+    <div class="relative rounded-full bg-blue-200 py-9 px-24">
+        <img
+            class="h-full drop-shadow-lg"
+            src="src/assets/under_maintenance.png"
+            alt="under_maintenance"
+        />
+    </div>
+</div>
