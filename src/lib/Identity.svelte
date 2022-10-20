f5482762e1219def1b39f84c3e9b25db200fb777
ch33kaboo <mc.ouzane@esi-sba.dz>
2022-10-20 18:46:28 +0100

    integrated Identity component in home page

--- src/lib/Identity.svelte
+++ src/lib/Identity.svelte
@@ -2,12 +2,15 @@
     // @ts-nocheck
     import Typewriter from 'svelte-typewriter';
 
-    export let identity = ['a club', 'a family', 'a community'];
+    export let identity = [' club', ' family', ' community'];
 </script>
 
-<Typewriter loop={1000} interval={50} cursor={false}>
-    {#each identity as id}
-        <span class="text-8xl">{id}.</span>
-    {/each}
-    {' '}
-</Typewriter>
+<div class="flex items-center justify-start">
+    a&nbsp;
+    <Typewriter loop={1000} interval={50} cursor={false}>
+        {#each identity as id}
+            <span class="text-8xl text-gray-900 dark:text-gray-100">{id}.</span>
+        {/each}
+        {' '}
+    </Typewriter>
+</div>
--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,7 +1,18 @@
 <script>
     import AnimatedLogo from '../lib/AnimatedLogo.svelte';
+    import Identity from '../lib/Identity.svelte';
 </script>
 
-<main class="flex w-full items-center justify-around">
-    <AnimatedLogo />
+<main class="my-10 grid w-full grid-cols-5 gap-5 font-semibold">
+    <div
+        class="col-span-3 flex items-center justify-start pl-24 text-8xl text-gray-900 dark:text-gray-100"
+    >
+        <div class="flex flex-col gap-4">
+            <h1>Alphabit is</h1>
+            <Identity />
+        </div>
+    </div>
+    <div class="ml-32 flex items-center justify-center">
+        <AnimatedLogo />
+    </div>
 </main>
