b4a8dd05cf1bb3a7d87f42bbdf4ffbb9de1e1e4c
ch33kaboo <mc.ouzane@esi-sba.dz>
2022-10-20 18:11:53 +0100

    added Identity component
    all this component does is show different words with typewriter effect, I used svelte-typewriter package in order to do so, I used version 2.4.5 instead of latest one because I found it easy to use, the new one did not work for some reasons, you can use this component in the home page for example to show alphabit identity in a cool way

---
+++ src/lib/Identity.svelte
@@ -0,0 +1,13 @@
+<script>
+    // @ts-nocheck
+    import Typewriter from 'svelte-typewriter';
+
+    export let identity = ['a club', 'a family', 'a community'];
+</script>
+
+<Typewriter loop={1000} interval={50} cursor={false}>
+    {#each identity as id}
+        <span class="text-8xl">{id}.</span>
+    {/each}
+    {' '}
+</Typewriter>
