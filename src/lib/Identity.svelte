00615828ad7558fae0bd489d7abf4563a9d15870
ch33kaboo <mc.ouzane@esi-sba.dz>
2022-10-21 14:37:26 +0100

    added delay before typewriter starts

--- src/lib/Identity.svelte
+++ src/lib/Identity.svelte
@@ -7,7 +7,7 @@
 
 <div class="flex items-center justify-start">
     a&nbsp;
-    <Typewriter loop={1000} interval={50} cursor={false}>
+    <Typewriter loop={1000} interval={50} cursor={false} delay={900}>
         {#each identity as id}
             <span
                 class="text-5xl text-gray-900 dark:text-gray-100 sm:text-7xl md:text-8xl lg:text-7xl xl:text-8xl"
