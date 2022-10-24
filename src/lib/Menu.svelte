36c2384d089419e01a411e111105c4fffbc2bdc4
ch33kaboo <mc.ouzane@esi-sba.dz>
2022-10-24 13:39:18 +0100

    done with the menu
    styled it and added items (links to routes)

--- src/lib/Menu.svelte
+++ src/lib/Menu.svelte
@@ -1,11 +1,23 @@
 <script>
+    import { Link } from 'svelte-navigator';
+
+    // importing configs
+    import routesList from '../config/routesList';
+
     export let menu;
 </script>
 
 <div
-    class="absolute top-24 bg-black transition-all duration-1000 lg:hidden {menu
-        ? 'left-0'
-        : '-left-24 opacity-0'}"
+    class="absolute top-24 z-10 flex flex-col gap-3 rounded-r-lg bg-indigo-300 py-3 pl-1 pr-3 text-xl capitalize transition-all duration-1000 dark:bg-gray-700 lg:hidden {menu
+        ? 'left-0 opacity-100'
+        : '-left-32 opacity-0'}"
 >
-    hahahaoooooo
+    <li class="px-2">
+        <Link to="/">Home</Link>
+    </li>
+    {#each routesList as route}
+        <li class="px-2">
+            <Link to="/{route}">{route}</Link>
+        </li>
+    {/each}
 </div>
