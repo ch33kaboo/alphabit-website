a95de6658b313bb0b815e554fcd6011ef07373a8
ch33kaboo <mc.ouzane@esi-sba.dz>
2022-10-24 13:45:15 +0100

    minor changes

--- src/config/routesList.js
+++ src/config/routesList.js
@@ -1,4 +1,5 @@
 // this file stores the routes of the website, if you want to add a route just change the array.
 // home route is not included in this array, it will be hard coded in the code.
+// the routes will be listed as ordered in the array.
 let routes = ['events', 'partners', 'blog', 'about'];
 export default routes;
