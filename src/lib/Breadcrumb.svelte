232ea645357ac0dc6b30d8dc1870ed67469c6cfd
ch33kaboo <mc.ouzane@esi-sba.dz>
2022-10-29 19:52:50 +0100

    showing routes on Breadcrumb, redirecting to routes still messed up

--- src/lib/Breadcrumb.svelte
+++ src/lib/Breadcrumb.svelte
@@ -1,3 +1,16 @@
+<script>
+    import { useNavigate, useLocation } from 'svelte-navigator';
+    const navigate = useNavigate();
+    const location = useLocation();
+    console.log($location.pathname.split('/').filter((n) => n !== ''));
+    let handleClick = (route) => {
+        let from =
+            ($location.state && $location.state.from) ||
+            `../../../../../../blog/${route}`;
+        navigate(`../../../../../../${route}`);
+    };
+</script>
+
 <div
     class="mt-1 mb-3 flex items-center justify-start text-gray-800 dark:text-blue-200"
 >
@@ -6,7 +19,9 @@
     >
         <ul>
             <li>
-                <a>
+                <!-- svelte-ignore a11y-click-events-have-key-events -->
+                <!-- svelte-ignore a11y-missing-attribute -->
+                <a on:click={() => handleClick('')}>
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
@@ -19,41 +34,33 @@
                             d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
                         /></svg
                     >
-                    Home
+                    Alphabit
                 </a>
             </li>
-            <li>
-                <a>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        class="mr-2 h-4 w-4 stroke-current"
-                        ><path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
-                        /></svg
-                    >
-                    Documents
-                </a>
-            </li>
-            <li>
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    class="mr-2 h-4 w-4 stroke-current"
-                    ><path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
-                    /></svg
-                >
-                Add Document
-            </li>
+
+            {#each $location.pathname
+                .split('/')
+                .filter((n) => n !== '') as item}
+                <li>
+                    <!-- svelte-ignore a11y-click-events-have-key-events -->
+                    <!-- svelte-ignore a11y-missing-attribute -->
+                    <a on:click={() => handleClick(item)}>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            class="mr-2 h-4 w-4 stroke-current"
+                            ><path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
+                            /></svg
+                        >
+                        {item}
+                    </a>
+                </li>
+            {/each}
         </ul>
     </div>
 </div>
