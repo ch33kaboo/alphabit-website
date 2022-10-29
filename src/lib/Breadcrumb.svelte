48a3e03240bfa1665856fb68f1ebf9049e165d7a
ch33kaboo <mc.ouzane@esi-sba.dz>
2022-10-29 15:02:59 +0100

    styled the Breadcrump

--- src/lib/Breadcrumb.svelte
+++ src/lib/Breadcrumb.svelte
@@ -1,7 +1,45 @@
-<div class="breadcrumbs text-sm">
-    <ul>
-        <li>
-            <a>
+<div
+    class="mt-1 mb-3 flex items-center justify-start text-gray-800 dark:text-blue-200"
+>
+    <div
+        class="breadcrumbs rounded-full border border-gray-800 bg-blue-200 px-4 text-sm dark:border-blue-200 dark:bg-gray-800"
+    >
+        <ul>
+            <li>
+                <a>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        class="mr-2 h-4 w-4 stroke-current"
+                        ><path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
+                        /></svg
+                    >
+                    Home
+                </a>
+            </li>
+            <li>
+                <a>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        class="mr-2 h-4 w-4 stroke-current"
+                        ><path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
+                        /></svg
+                    >
+                    Documents
+                </a>
+            </li>
+            <li>
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -11,43 +49,11 @@
                         stroke-linecap="round"
                         stroke-linejoin="round"
                         stroke-width="2"
-                        d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
+                        d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
                     /></svg
                 >
-                Home
-            </a>
-        </li>
-        <li>
-            <a>
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    class="mr-2 h-4 w-4 stroke-current"
-                    ><path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
-                    /></svg
-                >
-                Documents
-            </a>
-        </li>
-        <li>
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                class="mr-2 h-4 w-4 stroke-current"
-                ><path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
-                /></svg
-            >
-            Add Document
-        </li>
-    </ul>
+                Add Document
+            </li>
+        </ul>
+    </div>
 </div>
