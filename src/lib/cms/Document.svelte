a482d24160fc409b1565a4c22f7e7dfb01bc0526
ch33kaboo <mc.ouzane@esi-sba.dz>
2022-10-31 20:40:56 +0100

    added on load transition to Document component

--- src/lib/cms/Document.svelte
+++ src/lib/cms/Document.svelte
@@ -1,6 +1,8 @@
 <script>
     import SvelteMarkdown from 'svelte-markdown'; // to convert markdown to HTML
     import { textEmoji } from 'markdown-to-text-emoji'; // to convert the emoji codes (:smile: for example) the MD to an HTML emoji code
+    import { fly } from 'svelte/transition';
+    import { quintInOut } from 'svelte/easing';
 
     // importing envars
     const repo = import.meta.env.VITE_REPO;
@@ -25,6 +27,13 @@
     </div>
 {:then doc}
     <article
+        in:fly={{
+            y: 0,
+            duration: 500,
+            delay: 0,
+            opacity: 0,
+            easing: quintInOut
+        }}
         class="prose lg:prose-xl prose-p:px-5 prose-img:px-5 prose-video:px-5 prose-headings:px-5 prose-table:scale-90 md:prose-table:scale-100 my-8 
         text-slate-800 dark:text-gray-200 
         prose-headings:text-slate-800 dark:prose-headings:text-gray-200 
