132c11dae971ac2c934366e9533a000d7e7999bb
ch33kaboo <mc.ouzane@esi-sba.dz>
2022-11-09 22:33:26 +0100

    changed curved svg separator

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,14 +1,12 @@
-<div class="-mt-52 w-full">
+<div class="w-full">
     <svg
-        class="bg-fixed bg-center fill-slate-900 opacity-10 backdrop-blur-md dark:fill-gray-700 dark:opacity-50"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 1440 313"
+        preserveAspectRatio="none"
+        class="translate-y-0 bg-fixed bg-center fill-slate-900 opacity-10 backdrop-blur-md dark:fill-gray-700 dark:opacity-50"
+        viewBox="0 0 1440 74"
+        ><path
+            d="M456.464 0.0433865C277.158 -1.70575 0 50.0141 0 50.0141V74H1440V50.0141C1440 50.0141 1320.4 31.1925 1243.09 27.0276C1099.33 19.2816 1019.08 53.1981 875.138 50.0141C710.527 46.3727 621.108 1.64949 456.464 0.0433865Z"
+        /></svg
     >
-        <path
-            fill-opacity="1"
-            d="M0,288L120,282.7C240,277,480,267,720,266.7C960,267,1200,277,1320,282.7L1440,288L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"
-        />
-    </svg>
 
     <footer
         class="flex h-64 w-full select-none items-center justify-between overflow-x-hidden  bg-slate-900 bg-opacity-10 bg-[url('./src/assets/bg.svg')] bg-fixed bg-center backdrop-blur-md dark:bg-gray-700 dark:bg-opacity-50 dark:bg-[url('./src/assets/bg_dark.svg')]"
