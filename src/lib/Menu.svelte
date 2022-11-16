30531e1b0a96c51938ca62b222ec39554332be4c
ch33kaboo <mc.ouzane@esi-sba.dz>
2022-11-16 16:58:18 +0100

    changed the look of items inside the hamburger menu

--- src/lib/Menu.svelte
+++ src/lib/Menu.svelte
@@ -8,16 +8,42 @@
 </script>
 
 <div
-    class="fixed top-24 z-10 flex flex-col gap-3 rounded-r-lg bg-indigo-300 py-3 pl-1 pr-3 text-xl capitalize transition-all duration-1000 dark:bg-gray-700 lg:hidden {menu
+    class="fixed top-24 z-10 flex flex-col gap-3 rounded-r-lg bg-indigo-300 py-3 pl-1 pr-3 text-xl capitalize text-gray-900 transition-all duration-1000 dark:bg-gray-700 dark:text-white lg:hidden {menu
         ? 'left-0 opacity-100'
         : '-left-32 opacity-0'}"
 >
-    <li class="px-2">
+    <div class="flex justify-start gap-4 px-2">
+        <svg
+            class="h-4 w-4 translate-y-[6px]"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            ><path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M9 5l7 7-7 7"
+            /></svg
+        >
         <Link to="/">Home</Link>
-    </li>
+    </div>
     {#each routesList as route}
-        <li class="px-2">
+        <div class="flex justify-start gap-4 px-2">
+            <svg
+                class="h-4 w-4 translate-y-[6px]"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                ><path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M9 5l7 7-7 7"
+                /></svg
+            >
             <Link to="/{route.path}">{route.path}</Link>
-        </li>
+        </div>
     {/each}
 </div>
