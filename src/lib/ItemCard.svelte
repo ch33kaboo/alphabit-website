fc9b0ebc0f40f8390606ab65047477fb0831fea4
ch33kaboo <mc.ouzane@esi-sba.dz>
2022-11-25 14:48:35 +0100

    minor changes

--- src/lib/ItemCard.svelte
+++ src/lib/ItemCard.svelte
@@ -51,7 +51,7 @@
         delay: 200 * item.index + 1000,
         opacity: 0
     }}
-    class="group relative -mx-4 mb-6 grid h-[450px] transition-all hover:scale-[1.013] sm:-mx-0 sm:h-[210px]"
+    class="group relative -mx-4 mb-6 grid h-[450px] transition-all sm:-mx-0 sm:h-[210px] md:hover:scale-[1.013]"
 >
     <div
         class="absolute inset-y-2 -inset-x-1 bg-gradient-to-tr from-slate-700 to-slate-700 opacity-0 blur-xl transition-all {hovered
