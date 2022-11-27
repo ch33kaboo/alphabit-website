7e214ec3285b871d3d07efbdb99172c71642d510
ch33kaboo <mc.ouzane@esi-sba.dz>
2022-11-27 23:12:09 +0100

    started working on the first home sub section

--- src/lib/home_subs/HomeFirstSubSection.svelte
+++ src/lib/home_subs/HomeFirstSubSection.svelte
@@ -1,1 +1,50 @@
-<div class="w-full bg-red-700">hi first</div>
+<div
+    class="mt-12 flex w-full flex-col items-stretch justify-around md:flex-row"
+>
+    <div
+        class="flex max-w-[40%] flex-col items-center justify-center gap-2 text-center text-lg font-semibold"
+    >
+        <div class="mb-2 text-2xl font-bold">AlphaBit Club</div>
+        <div>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa
+            pariatur libero sit asperiores recusandae nostrum architecto animi
+            harum placeat omnis eos, minus delectus ducimus et quas, doloribus
+            cumque porro quam!
+        </div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui eos
+            repellat adipisci magni in.
+        </div>
+    </div>
+
+    <div class="w-5/12 p-1">
+        <div
+            class="group relative block overflow-hidden rounded-lg border border-gray-800 transition-all duration-500"
+        >
+            <div
+                class="absolute inset-0 z-[8] h-full w-full overflow-hidden bg-indigo-700 bg-fixed opacity-0 transition duration-300 ease-in-out group-hover:opacity-40"
+            />
+
+            <div
+                class="transition-all duration-500 group-hover:scale-105"
+                title=""
+            >
+                <!-- svelte-ignore a11y-missing-attribute -->
+                <img src="https://i.postimg.cc/7Y70QD9h/alpha-group.jpg" />
+            </div>
+            <div
+                class="absolute -bottom-52 right-2 left-2 z-[9] rounded bg-slate-900 p-4 transition-all duration-500 group-hover:bottom-2"
+            >
+                <div
+                    class="hover:text-primary-600 text-lg font-medium transition duration-500"
+                >
+                    AlphaBit members &hearts;
+                </div>
+                <p class="text-slate-400">
+                    picture taken on november 5th 2022, on the occasion of
+                    AlphaBit first general meeting.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
